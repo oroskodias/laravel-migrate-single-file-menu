40a2852b8f2e920e2fe3ad39954f4850d8548424
GitHub Action <action@github.com>
2022-03-14 22:53:46 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "laravel-migrate-single-file-menu"
---
+++ src/main/kotlin/com/github/oroskodias/laravelmigratesinglefilemenu/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.oroskodias.laravelmigratesinglefilemenu.services
+
+import com.github.oroskodias.laravelmigratesinglefilemenu.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
